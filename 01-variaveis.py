abef40fcba610a3e7b7529fb05e71e7fe4b99a75
gabrielaoliveirag105-wq <gabrielaoliveirag105@gmail.com>
2026-03-19 20:13:22 -0300

    Create 01-variaveis.py

---
+++ 01-variaveis.py
@@ -0,0 +1,13 @@
+# --- ALGORITMO (Visualg) ---
+# var msg: Caractere
+# msg <- "Olá, Mundo!"
+# escreval("Iniciando... ", msg)
+
+# --- CÓDIGO EM PYTHON ---
+# msg = "Olá, Mundo"
+# print(f"Iniciando... {msg}")
+
+# --- O QUE ACONTECE NO SISTEMA? ---
+# 1. Atribuição (=): O sinal '=' no Python e '<-' no Visualg é um comando de guardar aquele valor naquele momento. O '=' não significa igual em Python, ele é reconhecido como RECEBE. 
+# 2. Variável (msg): O computador guarda esse valor na memória, como uma caixinha.
+# 3. Exibição: O 'print' vai até essa caixinha e exibe o que está guardada nela.
