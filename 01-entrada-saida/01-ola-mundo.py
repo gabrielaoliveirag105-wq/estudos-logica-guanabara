4b348adbc7a48b97fc8cde37310869ff7dc0f105
gabrielaoliveirag105-wq <gabrielaoliveirag105@gmail.com>
2026-03-18 22:13:59 -0300

    Fix greeting and add comments in ola-mundo.py
    Corrected the greeting message and added comments explaining the code.

---
+++ 01-entrada-saida/01-ola-mundo.py
@@ -0,0 +1,13 @@
+# --- LÓGICA EM ALGORITMO (Visualg) ---
+# algoritmo "Ola_Mundo"
+# inicio
+#   Escreva("Olá, Mundo!")
+# fimalgoritmo
+
+# --- CÓDIGO EM PYTHON ---
+print("Wello, world!")
+
+# --- EXPLICAÇÃO DO FUNCIONAMENTO --- 
+# 1. O 'print' é a função de saída (escreva).
+# 2. O texto dentro das aspas é uma "String".
+# 3. O sistema reserva um pequeno espaço na memória para esse texto.
